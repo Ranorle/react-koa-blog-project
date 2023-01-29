90f130d611f5f7457aba923a31d42b7971d22546
Ranorle <869211109@qq.com>
2023-01-29 21:08:46 +0800

    完成文章删除

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,10 +1,11 @@
 import {db} from "../db.js"
+import jwt from "jsonwebtoken"
 export const getPosts=(req,res)=>{
     const q=req.query.cat ? "SELECT * FROM posts WHERE cat=?"
         :"SELECT * FROM posts"
 
     db.query(q,[req.query.cat],(err,data)=>{
-    if (err) return res.send(err)
+    if (err) return res.status(500).send(err)
         return res.status(200).json(data)
     })
 
@@ -22,8 +23,18 @@ export const addPost=(req,res)=>{
     res.json("from controller")
 }
 export const deletePost=(req,res)=>{
-    const token =req.cookies.access_cookie
+    const token =req.cookies.access_token
     if(!token) return res.status(401).json("没有相应权限！")
+    jwt.verify(token,"jwtkey",(err,userInfo)=>{
+    if(err) return res.status(403).json("身份认证不合法")
+
+    const postId =req.params.id
+    const q ="DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
+         db.query(q,[postId,userInfo.id],(err,data)=>{
+             if(err) return res.status(403).json("你只能删除你自己的文章")
+             return res.json("文章已被删除")
+         })
+    })
 }
 export const updatePost=(req,res)=>{
     res.json("from controller")
