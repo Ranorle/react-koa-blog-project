cdadfd0e73e214c2f617654f3738a2cc90d3c0d5
Ranorle <869211109@qq.com>
2023-01-29 20:44:12 +0800

    完成文章查询

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,30 @@
+import {db} from "../db.js"
+export const getPosts=(req,res)=>{
+    const q=req.query.cat ? "SELECT * FROM posts WHERE cat=?"
+        :"SELECT * FROM posts"
+
+    db.query(q,[req.query.cat],(err,data)=>{
+    if (err) return res.send(err)
+        return res.status(200).json(data)
+    })
+
+}
+export const getPost=(req,res)=>{
+    const q ="SELECT `username`, `title`, `desc`, p.img ,u.img AS userImg,`cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id= ?"
+    db.query(q,[req.params.id],(err,data)=>{
+        if(err) return res.status(500).json(err)
+        return res.status(200).json(data[0])
+    })
+}
+
+
 export const addPost=(req,res)=>{
+    res.json("from controller")
+}
+export const deletePost=(req,res)=>{
+    const token =req.cookies.access_cookie
+    if(!token) return res.status(401).json("没有相应权限！")
+}
+export const updatePost=(req,res)=>{
     res.json("from controller")
 }
